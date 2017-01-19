bc0d29b67197a3e7ed96a848b18267ad0f185945
Raphael Kohrs <KOHRS@KOHRS-DESKTOP.fritz.box>
2017-01-19 20:37:44 +0100

    Enable Action

--- Abfall/module.php
+++ Abfall/module.php
@@ -49,6 +49,7 @@ class KoAbfall extends IPSModule {
                 IPS_SetName($this->GetIDForIdent('muell' . $i), $this->ReadPropertyString('nameMuell' . $i));
             } elseif ($this->ReadPropertyBoolean('activeMuell' . $i) == 1 AND @ $this->GetIDForIdent('muell' . $i) === false) {
                 $this->RegisterVariableString('muell' . $i, $this->ReadPropertyString('nameMuell' . $i), "", $i);
+                $this->EnableAction('muell' . $i);
             } elseif ($this->ReadPropertyBoolean('activeMuell' . $i) == 0 AND @ $this->GetIDForIdent('muell' . $i) !== false) {
                 IPS_DeleteVariable($this->GetIDForIdent('muell' . $i));
             } else {
